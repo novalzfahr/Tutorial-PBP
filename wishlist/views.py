56d2f5329bdc668c41c292f0a5319cae4310788d
novalzfahr <novalzfahr22@gmail.com>
2022-09-15 15:03:41 +0700

    add new function

--- wishlist/views.py
+++ wishlist/views.py
@@ -21,10 +21,10 @@ def show_json(request):
     data = BarangWishlist.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
-def show_json_by_id(request):
+def show_json_by_id(request,id):
     data = BarangWishlist.objects.filter(pk=id)
-    #Jika JSON
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
-# // Jika XML
-# return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+def show_xml_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
